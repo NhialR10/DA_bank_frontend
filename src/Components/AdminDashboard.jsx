fa107e37f2274255e548d38bcbaf8261a185f026
NhialR10 <nhiallualcho@gmail.com>
2024-07-21 22:56:15 +0300

    reports section started

---
+++ src/Components/AdminDashboard.jsx
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from "react";
+import { formatNumber } from "../utils";
+const AdminDashboard = ({ branches }) => {
+  const [totalBalances, setTotalBalances] = useState({
+    dollarsBalance: 0,
+    sspBalance: 0,
+    ugxBalance: 0,
+    kenyaBalance: 0,
+  });
+
+  useEffect(() => {
+    const totals = branches.reduce(
+      (totals, branch) => {
+        totals.dollarsBalance += branch.dollarsAmount;
+        totals.sspBalance += branch.sspAmount;
+        totals.ugxBalance += branch.ugxAmount;
+        totals.kenyaBalance += branch.kshAmount;
+        return totals;
+      },
+      { dollarsBalance: 0, sspBalance: 0, ugxBalance: 0, kenyaBalance: 0 }
+    );
+
+    setTotalBalances(totals);
+  }, [branches]);
+  return (
+    <>
+      <div class="card text-bg-secondary" style={{ width: "100%" }}>
+        <div class="card-header">TOTAL AMOUNTS</div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>USD AMOUNT</span>{" "}
+              <span
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  fontWeight: "bold",
+                }}
+              >
+                {formatNumber(totalBalances.dollarsBalance)}
+              </span>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>SSP AMOUNT</span>{" "}
+              <span
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  fontWeight: "bold",
+                }}
+              >
+                {formatNumber(totalBalances.sspBalance)}
+              </span>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>KSH AMOUNT</span>{" "}
+              <span
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  fontWeight: "bold",
+                }}
+              >
+                {formatNumber(totalBalances.kenyaBalance)}
+              </span>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>UGX AMOUNT</span>{" "}
+              <span
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  fontWeight: "bold",
+                }}
+              >
+                {formatNumber(totalBalances.ugxBalance)}
+              </span>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="cards-container">
+        {branches.map((branch) => (
+          <div class="card text-bg-secondary" style={{ width: "23rem" }}>
+            <div class="card-header">{branch.name}</div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>USD AMOUNT</span>{" "}
+                  <span
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {formatNumber(branch.dollarsAmount)}
+                  </span>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>UGX AMOUNT</span>{" "}
+                  <span
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {formatNumber(branch.ugxAmount)}
+                  </span>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>KSH AMOUNT</span>{" "}
+                  <span
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {formatNumber(branch.kshAmount)}
+                  </span>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>SSP AMOUNT</span>{" "}
+                  <span
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {formatNumber(branch.sspAmount)}
+                  </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AdminDashboard;
--- src/Components/Reports.jsx
+++ src/Components/Reports.jsx
@@ -1,10 +1,65 @@
-import React from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "./AuthContext";
+import AddExpenses from "./AddExpenses";
+import ViewExpenses from "./ViewExpenses";
+import { BranchContext } from "./BranchContext";
+import CapitalOperation from "./CapitalOperation";
+import ViewCapitalOperation from "./ViewCapitalOperation";
+import AdminDashboard from "./AdminDashboard";
 const Reports = () => {
+  const [branches, setBranches] = useState([]);
+  const { fetchBranches } = useContext(BranchContext);
+  const [activeOperation, setActiveOperation] = useState(null);
+  const handleOperation = (operation) => {
+    setActiveOperation(operation);
+  };
+
+  useEffect(() => {
+    // Fetch Branches when component mounts
+    const getBranches = async () => {
+      try {
+        const fetchedBranches = await fetchBranches();
+        setBranches(fetchedBranches);
+      } catch (error) {
+        console.error("Error fetching branches:", error);
+      }
+    };
+
+    getBranches();
+  }, [fetchBranches]);
+
+  const renderOperationComponent = () => {
+    switch (activeOperation) {
+      case "adminDashBoard":
+        return <AdminDashboard branches={branches} />;
+      case "viewCapitalOperation":
+        return <ViewCapitalOperation />;
+
+      default:
+        return <div>Please select an operation</div>;
+    }
+  };
   return (
-    <main style={{ background: "red" }} className="main1-container">
-      <div>
-        <h1>Reports</h1>
+    <main className="main1-container">
+      <div className="setting-container">
+        <div className="setting-left">{renderOperationComponent()}</div>
+        <div className="setting-right">
+          <div className="operation-buttons">
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleOperation("adminDashBoard")}
+            >
+              Admin Dashboard
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleOperation("viewCapitalOperation")}
+            >
+              View Capital Operation
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
